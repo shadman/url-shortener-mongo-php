7f931c170d110a5cdc157388859dae5792e4cde0
Shadman <shadman.jamil@tenpearls.com>
2016-07-05 20:58:14 +0500

    added mongodb connection code to test

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = array(
+'username' => 'shadman',
+'password' => 'shan0321',
+'dbname'   => 'shortener-url',
+'connection_string'=> sprintf('mongodb://%s:%d/%s','mongo ds015690.mlab.com','15690','shortener-url')
+);
+
+
+$connection = new Mongo($config['connection_string']);
+
+$collection = $connection->shortener_url;
+
+$records = $collection->find();
+
+foreach ($records as $record) {
+	echo $record->url;
+}
