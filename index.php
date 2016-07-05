a3643ee4ae8a1099405c43b75025e0ebe7ea672d
Shadman <shadman.jamil@tenpearls.com>
2016-07-06 00:40:25 +0500

    updated db connection for heroku test

--- index.php
+++ index.php
@@ -4,15 +4,17 @@ error_reporting(E_ALL);
 $config = array(
 'username' => 'shadman',
 'password' => 'shan0321',
-'dbname'   => 'shortener-url',
-'connection_string'=> sprintf('mongodb://%s:%d/%s','mongo ds015690.mlab.com','15690','shortener-url')
+'db'   => 'shortener-url',
+'connection_string'=> sprintf('mongodb://%s:%d','ds015690.mlab.com','15690')
 );
 
 #mongo ds015690.mlab.com:15690/shortener-url -u shadman -p shan0321
 
-$connection = new MongoClient($config['connection_string']);
+$connection = new MongoClient($config['connection_string'], array("username" => $config['username'], "password" => $config['password'], "db" => $config['db']));
 
-$collection = $connection->shortener_url;
+//$collection = $connection->shortener_url;
+//$collection = $connection->selectCollection($connection['dbname'], 'shortener_url');
+$collection = $connection->selectDB('shortener_url')->selectCollection('shortener_url');
 
 $records = $collection->find();
 
