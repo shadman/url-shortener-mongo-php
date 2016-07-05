2c102b3362602868e8c075271635eb4b6c482734
Shadman <shadman.jamil@tenpearls.com>
2016-07-05 22:32:15 +0500

    added mongo client instead of mongo class

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 error_reporting(E_ALL);
 
-getenv("MONGO_URL");
-
 $config = array(
 'username' => 'shadman',
 'password' => 'shan0321',
 'dbname'   => 'shortener-url',
-'connection_string'=> sprintf('mongodb://%s:%d/%s','mongo ds015690.mlab.com','15690','shortener-url')
+'connection_string'=> sprintf('mongodb://%s:%d/%s -u %s -p %s','mongo ds015690.mlab.com','15690','shortener-url','shadman','shan0321')
 );
 
+#mongo ds015690.mlab.com:15690/shortener-url -u shadman -p shan0321
 
-$connection = new Mongo($config['connection_string']);
+$connection = new MongoClient($config['connection_string']);
 
 $collection = $connection->shortener_url;
 
