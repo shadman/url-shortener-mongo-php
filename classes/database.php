f2a3766512d052133a13fc8190a33a0bb54bf1dc
Shadman <shadman.jamil@tenpearls.com>
2016-07-07 01:06:03 +0500

    created database class to access mongodb

---
+++ classes/database.php
@@ -0,0 +1,38 @@
+<?php
+class Database {
+
+	public $connection = '';
+
+	# Creating a connection string for MongoDB
+	public static function connectionString(){
+		return sprintf('mongodb://%s:%d', Config::$DB_HOST, Config::$DB_PORT);
+	}
+
+
+	public function __construct(){
+
+		try {
+			$this->connect = new MongoClient(self::connectionString(), 
+										  array("username" => Config::$DB_USERNAME, "password" => Config::$DB_PASSWORD, "db" => Config::$DB_NAME) );
+			$this->connection = $this->connect->selectDB(Config::$DB_NAME);
+		} catch (Exception $e) {
+		    echo $e->getMessage();
+		}
+
+	}
+	
+
+	public function getCollection($collection_name){
+		try {
+			$collection =  new MongoCollection($this->connection, Config::$COLLECTION_NAMES[$collection_name]);
+			return $collection;
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+}
+
+global $database;
+$database = new Database;
+
