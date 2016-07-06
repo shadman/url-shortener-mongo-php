cf637598c0c8310ba96e93cd8b323c2f8171637b
Shadman <shadman.jamil@tenpearls.com>
2016-07-07 01:05:15 +0500

    added config file

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+class Config {
+
+	# Database 
+    static $DB_HOST    				= 'ds015690.mlab.com';
+    static $DB_PORT    				= '15690';
+    static $DB_NAME      			= 'shortener-url';
+    static $DB_USERNAME  			= 'shadman';
+    static $DB_PASSWORD  		 	= 'shan0321';
+
+    # All collection names 
+    static $COLLECTION_NAMES    	= array(
+    										'shortener_url' => 'shortener_url' 
+    								  );
+
+
+}
