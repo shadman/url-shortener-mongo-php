04c1007225d4d71f06878599f89dac8caa5edaa3
Shadman <shadman.jamil@tenpearls.com>
2016-07-07 13:23:51 +0500

    updated styles by adding css file; concerns integrated

--- index.php
+++ index.php
@@ -1,42 +1,31 @@
 <?php
 require 'config/config.php';
 require 'classes/database.php';
+
+require 'classes/application.php';
 require 'models/shortener_url.php';
 
-
-	# If form posted
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-		$shortenerURL = new shortenerURL;
-		$response = $shortenerURL->createShortURL($_POST['url']);
-		if (is_array($response)===true) {
-			$msg = "Your short url: ". 'http://localhost/'. $response['short_url'];
-		}
-
-	}
-	else if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['short_code']) {
-		//redirect to desired url
-		$short_code = $_GET['short_code'];
-		$shortenerURL = new shortenerURL;
-		$record = $shortenerURL->getRecord($short_code);
-
-		header( "Location: ".$record['url'] );
-		exit;
-	}
+	require 'concerns/process.php';
 
 ?>
 <html>
 <head>
+	<link rel="stylesheet" href="styles/style.css">
+	<title>Shortener URL Application</title>
 </head>
 <body>
 <form action="" method="post">
 
-	<input type="text" name="url" placeholder="http://example.com" class="url-box">
-	<input type="submit" value="Generate Short URL">
+	<input type="text" name="url" placeholder="http://example.com" class="url-box"><br />
+	<input type="submit" value="Generate Short URL" class="generate-button">
 
-</form> 
+</form>
 
-<?php echo $msg; ?>
+	<?php if (isset($msg)) { ?>
+		<div class="message-area">
+			<?php echo $msg; ?>
+		</div>
+	<?php } ?>
 
 </body>
 </html>
